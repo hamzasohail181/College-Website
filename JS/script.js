a85c9ad78fba25d2d767b8a01e82957de49490cb
hamzasohail181 <hamzasohail867gmail.com>
2025-12-03 15:11:55 +0500

    reemove Hamburger

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,8 @@
 // ===== MOBILE MENU TOGGLE =====
-const menuToggle = document.querySelector('.menu-toggle');
+
 const navLinks = document.querySelector('.nav-links');
 
-if(menuToggle) {
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-}
+
 
 // ===== SIMPLE FORM VALIDATION =====
 const forms = document.querySelectorAll('form');
